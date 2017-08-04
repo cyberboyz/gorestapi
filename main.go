1bd7779da52f2a12339f8b48101be7738667985b
Bismo Baruno <bismobaruno@gmail.com>
2017-08-04 09:01:11 +0700

    change port value

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"binar-academy/example-db-rest-api/routers"
+	"os"
 )
 
-const SERVER_PORT string = ":8080"
+const SERVER_PORT string = "8080"
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = SERVER_PORT
+	}
+
 	r := routers.GetEngine()
-	r.Run(SERVER_PORT)
+	r.Run(":" + port)
 }
